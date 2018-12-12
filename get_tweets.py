280972d8884b718e74e33fd2d3f5e2a7d895b915
yuyurun <y.hashizume@mibel.cs.tsukuba.ac.jp>
2018-12-12 13:56:22 +0900

    ツイートの取得

---
+++ get_tweets.py
@@ -0,0 +1,17 @@
+# coding : UTF-8
+
+import settings
+import tweepy
+
+consumer_key    = settings.CK
+consumer_secret = settings.CS
+access_key      = settings.AK
+access_secret   = settings.AS
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+tweets = api.search(q='dip株式会社', count=10)
+for tweet in tweets:
+    print(tweet.text, "/n")
