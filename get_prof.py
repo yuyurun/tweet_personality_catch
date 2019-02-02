5b26a12c802776c926afc1eadc704ba674ef1b73
yuyurun <yuyu1.run@icloud.com>
2019-02-02 22:41:48 +0900

    プロフィールのアカウント名：標準入力

--- get_prof.py
+++ get_prof.py
@@ -1,5 +1,6 @@
 import urllib.request as request
 import re
+import argparse
 
 def twitProf(usrId):
   res = request.urlopen('https://twitter.com/'+usrId)
@@ -11,6 +12,10 @@ def twitProf(usrId):
   return m.group(1)
 
 if __name__ == '__main__':
-  id = 'yuyurun'
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-u', '--user_name', action ='store')
+  args = parser.parse_args()
+  username = args.user_name
+  id = username
   print('id :',id)
   print('profile :',twitProf(id))
