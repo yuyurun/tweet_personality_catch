d16903cec9bfcb373759e84c2c3c3a3e8ed71b6b
yuyurun <yuyu1.run@icloud.com>
2019-03-22 18:02:23 +0900

    t2t用のword,docセットのcsv

--- train_meisi.py
+++ train_meisi.py
@@ -39,6 +39,30 @@ def mecab_word_meisi(texts):
     output = []
 
 
+def mecab_word_meisi_csv(texts):
+  output = []
+  f = open(args.input_path + ".1080.csv","w")
+  f.write("word , doc \n")
+  for text in texts:
+    tagger = mc.Tagger('-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/')
+    tagger.parse('')
+    #enc_text = text.encode('utf-8')
+    result = tagger.parseToNode(text)
+    while(result):
+      if result.surface != "":  # ヘッダとフッタを除外
+        word_type = result.feature.split(",")[0]
+        if word_type in ["名詞"]:
+          output.append(result.surface)
+      result = result.next
+      if result is None:
+        break
+    if not "," in text:
+      if len(text) < 80 and len(text) > 10:
+        for word in output:
+          f.write(word + " ")
+        f.write("," + text)
+        #f.write("\n")
+    output = []
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -49,5 +73,6 @@ if __name__ == "__main__":
   texts = word_cloud.load_data(input_file)
   #output = word_cloud.mecab_word(texts)
   #word_count(output)
-  mecab_word_meisi(texts)
+  #mecab_word_meisi(texts)
+  mecab_word_meisi_csv(texts)
   #out_text(output)
